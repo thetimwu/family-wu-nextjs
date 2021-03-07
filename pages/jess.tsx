8a0847a06154baff65c510fab2f87dc79b73ab3b
tim wu <timwuuk@gmail.com>
2021-03-07 16:18:30 +0000

    responsive design for Tim and Jess

--- pages/jess.tsx
+++ pages/jess.tsx
@@ -4,19 +4,21 @@ import Layout from "../components/layout/Layout";
 const jess: React.FC = () => {
   return (
     <Layout color="red">
-      <div className="min-h-screen bg-gradient-to-b from-red-100 to-red-400 flex items-center">
-        <div className="min-h-screen flex flex-row bg-gray-100">
-          <div className="flex flex-col w-56 bg-white overflow-hidden">
+      <div className="min-h-full bg-gradient-to-b from-red-100 to-red-400 flex items-center">
+        <div className="min-h-full flex flex-row bg-gray-100">
+          <div className="flex flex-col md:w-40 lg:w-56 bg-white overflow-hidden">
             <div className="flex items-center justify-center h-20 shadow-md">
-              <h1 className="text-3xl uppercase text-red-500">Jessica</h1>
+              <h1 className="md:text-xl lg:text-3xl uppercase text-red-500">
+                Jessica
+              </h1>
             </div>
             <ul className="flex flex-col py-4">
               <li>
                 <a
                   href="#"
-                  className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
+                  className="flex flex-row items-center h-12 transform hover:translate-x-3 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
                 >
-                  <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-red-400">
+                  <span className="inline-flex items-center justify-center h-12 w-4 sm:w-6 md:w-12 text-lg text-red-400">
                     <i className="bx bx-home"></i>
                   </span>
                   <span className="text-sm font-medium">Dashboard</span>
@@ -25,9 +27,9 @@ const jess: React.FC = () => {
               <li>
                 <a
                   href="#"
-                  className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
+                  className="flex flex-row items-center h-12 transform hover:translate-x-3 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
                 >
-                  <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-red-400">
+                  <span className="inline-flex items-center justify-center h-12 w-4 sm:w-6 md:w-12 text-lg text-red-400">
                     <i className="bx bx-music"></i>
                   </span>
                   <span className="text-sm font-medium">Music</span>
@@ -36,9 +38,9 @@ const jess: React.FC = () => {
               <li>
                 <a
                   href="#"
-                  className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
+                  className="flex flex-row items-center h-12 transform hover:translate-x-3 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
                 >
-                  <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-red-400">
+                  <span className="inline-flex items-center justify-center h-12 w-4 sm:w-6 md:w-12 text-lg text-red-400">
                     <i className="bx bx-drink"></i>
                   </span>
                   <span className="text-sm font-medium">Drink</span>
@@ -47,9 +49,9 @@ const jess: React.FC = () => {
               <li>
                 <a
                   href="#"
-                  className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
+                  className="flex flex-row items-center h-12 transform hover:translate-x-3 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
                 >
-                  <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-red-400">
+                  <span className="inline-flex items-center justify-center h-12 w-4 sm:w-6 md:w-12 text-lg text-red-400">
                     <i className="bx bx-shopping-bag"></i>
                   </span>
                   <span className="text-sm font-medium">Shopping</span>
@@ -58,9 +60,9 @@ const jess: React.FC = () => {
               <li>
                 <a
                   href="#"
-                  className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
+                  className="flex flex-row items-center h-12 transform hover:translate-x-3 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
                 >
-                  <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-red-400">
+                  <span className="inline-flex items-center justify-center h-12 w-4 sm:w-6 md:w-12 text-lg text-red-400">
                     <i className="bx bx-chat"></i>
                   </span>
                   <span className="text-sm font-medium">Chat</span>
@@ -69,9 +71,9 @@ const jess: React.FC = () => {
               <li>
                 <a
                   href="#"
-                  className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
+                  className="flex flex-row items-center h-12 transform hover:translate-x-3 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
                 >
-                  <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-red-400">
+                  <span className="inline-flex items-center justify-center h-12 w-4 sm:w-6 md:w-12 text-lg text-red-400">
                     <i className="bx bx-user"></i>
                   </span>
                   <span className="text-sm font-medium">Profile</span>
@@ -80,9 +82,9 @@ const jess: React.FC = () => {
               <li>
                 <a
                   href="#"
-                  className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
+                  className="flex flex-row items-center h-12 transform hover:translate-x-3 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
                 >
-                  <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-red-400">
+                  <span className="inline-flex items-center justify-center h-12 w-4 sm:w-6 md:w-12 text-lg text-red-400">
                     <i className="bx bx-bell"></i>
                   </span>
                   <span className="text-sm font-medium">Notifications</span>
@@ -91,9 +93,9 @@ const jess: React.FC = () => {
               <li>
                 <a
                   href="#"
-                  className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
+                  className="flex flex-row items-center h-12 transform hover:translate-x-3 transition-transform ease-in duration-200 text-red-400 hover:text-red-400"
                 >
-                  <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-red-400">
+                  <span className="inline-flex items-center justify-center h-12 w-4 sm:w-6 md:w-12 text-lg text-red-400">
                     <i className="bx bx-log-out"></i>
                   </span>
                   <span className="text-sm font-medium">Logout</span>
@@ -102,6 +104,7 @@ const jess: React.FC = () => {
             </ul>
           </div>
         </div>
+        <div>content</div>
       </div>
     </Layout>
   );
